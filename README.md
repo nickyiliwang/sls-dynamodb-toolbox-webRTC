4ee7be34d8be7a57cb2ab349b4460bcd566e1429
nickyiliwang <nickyiliwang@gmail.com>
2022-04-03 21:06:59 -0400

    inital with updated readme, wip client with vanilia js and html

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # webRTC_terraform
+
+Initial plan
+1. Following a guide to learn the basics
+2. See if I can provision an EC2 instance with docker installed and setup an signaling server running with peerJS at port 9000
+3. Write the terraform template for it
+
+Credits:
+1. [WebRTC in 100 Seconds](https://www.youtube.com/watch?v=WmR9IMUD_CY)
---
+++ server/client/script.js
@@ -0,0 +1,21 @@
+let pc = new RTCPeerConnection(servers);
+
+// ICE
+const servers = {
+  iceServers: [
+    { urls: ["stun1.l.google.com:19302", "stun2.l.google.com:19302"] },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+let localStream = null;
+let remoteStream = null;
+
+// HTML elements
+const webcamButton = document.getElementById('webcamButton');
+const webcamVideo = document.getElementById('webcamVideo');
+const callButton = document.getElementById('callButton');
+const callInput = document.getElementById('callInput');
+const answerButton = document.getElementById('answerButton');
+const remoteVideo = document.getElementById('remoteVideo');
+const hangupButton = document.getElementById('hangupButton');
